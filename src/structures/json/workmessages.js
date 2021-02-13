0b2b8d4c2047d92d33d4c08cabee4ac7ffa65d24
Hayden Roy <78992586+SlavenCrown@users.noreply.github.com>
2021-02-12 20:00:22 -0800

    added pets and work messages

--- src/structures/json/workmessages.js
+++ src/structures/json/workmessages.js
@@ -141,16 +141,16 @@ module.exports.FinalWorkMessages = {
     },
     techsupport: {
         perfect: [
-            "",
-            ""
+            "You successfully secured somebodies OS!",
+            "You did such an amazing job you got a bonus from your employer!"
         ],
         good: [
-            "",
-            ""
+            "You helped an elderly person exit fullscreen.",
+            "You fixed a bug on a solitare website."
         ],
         bad: [
-            "",
-            ""
+            "You stole somebodies SSN.",
+            "You ended up downloading more malware instead of getting rid of it."
         ]
     },
     softwaredeveloper: {
