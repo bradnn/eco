bd15a408f11112c48f7d683ac932d1b69d1c57b9
CrypticComet <iiepicnexus@icloud.com>
2021-02-13 16:36:23 -0800

    Added New Words To Unscramble

--- src/structures/json/unscramble.js
+++ src/structures/json/unscramble.js
@@ -1,9 +1,15 @@
 module.exports.UnscrambleWords = [
-    "economy", 
-    "stocks", 
-    "money", 
-    "cash", 
-    "work", 
-    "gamer", 
-    "janitor"
+    "Economy", 
+    "Stocks", 
+    "Money", 
+    "Cash", 
+    "Work", 
+    "Gamer", 
+    "Janitor",
+    "Rich",
+    "Discord",
+    "Time",
+    "Life",
+    "Company",
+    "Profit",
 ];
