84683436b7c97ef838cae73c1803bda22741fa3e
Hayden Roy <78992586+SlavenCrown@users.noreply.github.com>
2021-02-14 13:49:17 -0800

    Added Hunting Items

---
+++ src/resources/items/hunting/bow.js
@@ -0,0 +1,55 @@
+const userModel = require('../../../structures/models/User.js');
+
+module.exports = class {
+    constructor() {
+        this.id = '012',
+
+        this.name = 'bow',
+        this.formatName = 'Bow',
+        this.emoji = '🏹',
+
+        this.currency = 'coins',
+        this.buyPrice = '1500000',
+        this.sellPrice = '0',
+        this.purchasable = true,
+
+        this.category = 'hunting',
+        this.categoryName = 'Hunting'
+    }
+
+    async add(userID, amount) {
+        let res = await userModel.findOne({userID: userID}, async function (err, res) {
+            if (err) throw err;
+            if (res) {
+                return res;
+            }
+        });
+
+        if (!res) {
+            res = await userModel.create({
+                userID: userID
+            });
+        }
+
+        res.collections[this.category][this.name] += amount;
+        res.save();
+    }
+
+    async remove(userID, amount) {
+        let res = await userModel.findOne({userID: userID}, async function (err, res) {
+            if (err) throw err;
+            if (res) {
+                return res;
+            }
+        });
+
+        if (!res) {
+            res = await userModel.create({
+                userID: userID
+            });
+        }
+
+        res.collections[this.category][this.name] -= amount;
+        res.save();
+    }
+}
