b768c913479b47835f7eea7f35e613cf02a70c20
Hayden Roy <78992586+SlavenCrown@users.noreply.github.com>
2021-02-14 13:39:35 -0800

    Added all car items

---
+++ src/resources/items/cars/bugattidivo.js
@@ -0,0 +1,55 @@
+const userModel = require('../../../structures/models/User.js');
+
+module.exports = class {
+    constructor() {
+        this.id = '',
+
+        this.name = 'bugattiDivo',
+        this.formatName = 'Bugatti Divo',
+        this.emoji = '🚗',
+
+        this.currency = 'coins',
+        this.buyPrice = '5400000',
+        this.sellPrice = '3250000',
+        this.purchasable = true,
+
+        this.category = 'cars',
+        this.categoryName = 'Cars'
+    }
+
+    async add(userID, amount) {
+        let res = await userModel.findOne({userID: userID}, async function (err, res) {
+            if (err) throw err;
+            if (res) {
+                return res;
+            }
+        });
+
+        if (!res) {
+            res = await userModel.create({
+                userID: userID
+            });
+        }
+
+        res.collections[this.category][this.name] += amount;
+        res.save();
+    }
+
+    async remove(userID, amount) {
+        let res = await userModel.findOne({userID: userID}, async function (err, res) {
+            if (err) throw err;
+            if (res) {
+                return res;
+            }
+        });
+
+        if (!res) {
+            res = await userModel.create({
+                userID: userID
+            });
+        }
+
+        res.collections[this.category][this.name] -= amount;
+        res.save();
+    }
+}
---
+++ src/resources/items/cars/bugattiveyron.js
@@ -0,0 +1,55 @@
+const userModel = require('../../../structures/models/User.js');
+
+module.exports = class {
+    constructor() {
+        this.id = '',
+
+        this.name = 'bugattiVeyron',
+        this.formatName = 'Bugatti Veyron',
+        this.emoji = '🚗',
+
+        this.currency = 'coins',
+        this.buyPrice = '2000000',
+        this.sellPrice = '1500000',
+        this.purchasable = true,
+
+        this.category = 'cars',
+        this.categoryName = 'Cars'
+    }
+
+    async add(userID, amount) {
+        let res = await userModel.findOne({userID: userID}, async function (err, res) {
+            if (err) throw err;
+            if (res) {
+                return res;
+            }
+        });
+
+        if (!res) {
+            res = await userModel.create({
+                userID: userID
+            });
+        }
+
+        res.collections[this.category][this.name] += amount;
+        res.save();
+    }
+
+    async remove(userID, amount) {
+        let res = await userModel.findOne({userID: userID}, async function (err, res) {
+            if (err) throw err;
+            if (res) {
+                return res;
+            }
+        });
+
+        if (!res) {
+            res = await userModel.create({
+                userID: userID
+            });
+        }
+
+        res.collections[this.category][this.name] -= amount;
+        res.save();
+    }
+}
